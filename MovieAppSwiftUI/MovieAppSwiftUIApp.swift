7251ba946421157fa84a33772a2fc601e687d51d
jswalsakshi <my@pers.on.al>
2023-12-12 11:39:16 +0530

    Initial Commit

---
+++ MovieAppSwiftUI/MovieAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppSwiftUIApp.swift
+//  MovieAppSwiftUI
+//
+//  Created by sakshi.j on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
