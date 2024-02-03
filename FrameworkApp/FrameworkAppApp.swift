310a7168745fc7e79e467eb39918af9b31051d61
jswalsakshi <my@pers.on.al>
2024-02-03 10:01:11 +0530

    Initial Commit

---
+++ FrameworkApp/FrameworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkAppApp.swift
+//  FrameworkApp
+//
+//  Created by sakshi.j on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
