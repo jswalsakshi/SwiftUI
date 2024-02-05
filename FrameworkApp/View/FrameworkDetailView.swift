38b752c8d677cd29a64351733d6d2b22808f2d7a
jswalsakshi <my@pers.on.al>
2024-02-05 15:15:52 +0530

    Framework App in SwiftUI

---
+++ FrameworkApp/View/FrameworkDetailView.swift
@@ -0,0 +1,57 @@
+//
+//  FrameworkDetailView.swift
+//  FrameworkApp
+//
+//  Created by sakshi.j on 05/02/24.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    let framework: Framework
+    var body: some View {
+        VStack {
+            
+            HStack {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
+            AFButton(title: "Learn More")
+        }
+    }
+}
+
+#Preview {
+    FrameworkDetailView(framework: MockData.sampleFramework)
+}
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Button(title) {
+            
+        }
+        .font(.title2)
+        .fontWeight(.semibold)
+        .frame(width: 280, height: 50)
+        .background(.red)
+        .foregroundColor(.white)
+        .cornerRadius(10.0)
+    }
+}
